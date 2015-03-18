c8fccfb99b8090ec6f11ac0f4d7d2f80c0a49a3b
nokipls <nokimrn@gmail.com>
2015-03-18 17:11:07 +0100

    Semi-fix

---
+++ org.eclipse.acceleo.module.ase/Cs.cs
@@ -0,0 +1,173 @@
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<xmi:XMI xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:cs="http://cs/1.0">
+  <cs:MonitoredCity name="Milano">
+    <stats xsi:type="cs:NumPost" name="#Posts" metric="3"/>
+    <stats xsi:type="cs:NumPhoto" name="#Pictures" metric="1"/>
+    <stats xsi:type="cs:NumPostLang" name="#Posts per language">
+      <langNumber value="0"/>
+      <langNumber key="English" value="1"/>
+      <langNumber key="LassaSta" value="2"/>
+    </stats>
+    <stats xsi:type="cs:NumPostTimeSlot" name="#Posts per time slot">
+      <postsPerTimeSlot value="0"/>
+      <postsPerTimeSlot key="Afternoon" value="0"/>
+      <postsPerTimeSlot key="Evening" value="0"/>
+      <postsPerTimeSlot key="Night" value="1"/>
+    </stats>
+    <stats xsi:type="cs:CategoriesCount" name="#Posts per Category">
+      <catNumber value="0"/>
+      <catNumber key="Food" value="0"/>
+      <catNumber key="Fun" value="0"/>
+    </stats>
+  </cs:MonitoredCity>
+  <cs:District name="SudOvest" coordEx="1.0" coordEy="1.0">
+    <stats xsi:type="cs:NumPost" name="#Posts" metric="2"/>
+    <stats xsi:type="cs:NumPhoto" name="#Pictures" metric="1"/>
+    <stats xsi:type="cs:NumPostLang" name="#Posts per language">
+      <langNumber value="0"/>
+      <langNumber key="English" value="1"/>
+      <langNumber key="LassaSta" value="1"/>
+    </stats>
+    <stats xsi:type="cs:NumPostTimeSlot" name="#Posts per time slot">
+      <postsPerTimeSlot value="0"/>
+      <postsPerTimeSlot key="Afternoon" value="0"/>
+      <postsPerTimeSlot key="Evening" value="0"/>
+      <postsPerTimeSlot key="Night" value="1"/>
+    </stats>
+  </cs:District>
+  <cs:District name="SudCentro" coordBx="1.0" coordEx="2.0">
+    <stats xsi:type="cs:NumPost" name="#Posts"/>
+    <stats xsi:type="cs:NumPhoto" name="#Pictures"/>
+    <stats xsi:type="cs:NumPostLang" name="#Posts per language">
+      <langNumber value="0"/>
+      <langNumber key="English" value="0"/>
+      <langNumber key="LassaSta" value="0"/>
+    </stats>
+    <stats xsi:type="cs:NumPostTimeSlot" name="#Posts per time slot">
+      <postsPerTimeSlot value="0"/>
+      <postsPerTimeSlot key="Afternoon" value="0"/>
+      <postsPerTimeSlot key="Evening" value="0"/>
+      <postsPerTimeSlot key="Night" value="0"/>
+    </stats>
+  </cs:District>
+  <cs:District name="SudEst" coordBx="2.0" coordEx="3.0" coordEy="1.0">
+    <stats xsi:type="cs:NumPost" name="#Posts"/>
+    <stats xsi:type="cs:NumPhoto" name="#Pictures"/>
+    <stats xsi:type="cs:NumPostLang" name="#Posts per language">
+      <langNumber value="0"/>
+      <langNumber key="English" value="0"/>
+      <langNumber key="LassaSta" value="0"/>
+    </stats>
+    <stats xsi:type="cs:NumPostTimeSlot" name="#Posts per time slot">
+      <postsPerTimeSlot value="0"/>
+      <postsPerTimeSlot key="Afternoon" value="0"/>
+      <postsPerTimeSlot key="Evening" value="0"/>
+      <postsPerTimeSlot key="Night" value="0"/>
+    </stats>
+  </cs:District>
+  <cs:District name="CentroOvest" coordBy="1.0" coordEx="1.0" coordEy="2.0">
+    <stats xsi:type="cs:NumPost" name="#Posts"/>
+    <stats xsi:type="cs:NumPhoto" name="#Pictures"/>
+    <stats xsi:type="cs:NumPostLang" name="#Posts per language">
+      <langNumber value="0"/>
+      <langNumber key="English" value="0"/>
+      <langNumber key="LassaSta" value="0"/>
+    </stats>
+    <stats xsi:type="cs:NumPostTimeSlot" name="#Posts per time slot">
+      <postsPerTimeSlot value="0"/>
+      <postsPerTimeSlot key="Afternoon" value="0"/>
+      <postsPerTimeSlot key="Evening" value="0"/>
+      <postsPerTimeSlot key="Night" value="0"/>
+    </stats>
+  </cs:District>
+  <cs:District name="CentroCentro" coordBx="1.0" coordBy="1.0" coordEx="2.0" coordEy="2.0">
+    <stats xsi:type="cs:NumPost" name="#Posts" metric="1"/>
+    <stats xsi:type="cs:NumPhoto" name="#Pictures"/>
+    <stats xsi:type="cs:NumPostLang" name="#Posts per language">
+      <langNumber value="0"/>
+      <langNumber key="English" value="0"/>
+      <langNumber key="LassaSta" value="1"/>
+    </stats>
+    <stats xsi:type="cs:NumPostTimeSlot" name="#Posts per time slot">
+      <postsPerTimeSlot value="0"/>
+      <postsPerTimeSlot key="Afternoon" value="0"/>
+      <postsPerTimeSlot key="Evening" value="0"/>
+      <postsPerTimeSlot key="Night" value="0"/>
+    </stats>
+  </cs:District>
+  <cs:District name="CentroEst" coordBx="2.0" coordBy="1.0" coordEx="3.0" coordEy="2.0">
+    <stats xsi:type="cs:NumPost" name="#Posts"/>
+    <stats xsi:type="cs:NumPhoto" name="#Pictures"/>
+    <stats xsi:type="cs:NumPostLang" name="#Posts per language">
+      <langNumber value="0"/>
+      <langNumber key="English" value="0"/>
+      <langNumber key="LassaSta" value="0"/>
+    </stats>
+    <stats xsi:type="cs:NumPostTimeSlot" name="#Posts per time slot">
+      <postsPerTimeSlot value="0"/>
+      <postsPerTimeSlot key="Afternoon" value="0"/>
+      <postsPerTimeSlot key="Evening" value="0"/>
+      <postsPerTimeSlot key="Night" value="0"/>
+    </stats>
+  </cs:District>
+  <cs:District name="NordOvest" coordBy="2.0" coordEx="1.0" coordEy="3.0">
+    <stats xsi:type="cs:NumPost" name="#Posts"/>
+    <stats xsi:type="cs:NumPhoto" name="#Pictures"/>
+    <stats xsi:type="cs:NumPostLang" name="#Posts per language">
+      <langNumber value="0"/>
+      <langNumber key="English" value="0"/>
+      <langNumber key="LassaSta" value="0"/>
+    </stats>
+    <stats xsi:type="cs:NumPostTimeSlot" name="#Posts per time slot">
+      <postsPerTimeSlot value="0"/>
+      <postsPerTimeSlot key="Afternoon" value="0"/>
+      <postsPerTimeSlot key="Evening" value="0"/>
+      <postsPerTimeSlot key="Night" value="0"/>
+    </stats>
+  </cs:District>
+  <cs:District name="NordCentro" coordBx="1.0" coordBy="2.0" coordEx="2.0" coordEy="3.0">
+    <stats xsi:type="cs:NumPost" name="#Posts"/>
+    <stats xsi:type="cs:NumPhoto" name="#Pictures"/>
+    <stats xsi:type="cs:NumPostLang" name="#Posts per language">
+      <langNumber value="0"/>
+      <langNumber key="English" value="0"/>
+      <langNumber key="LassaSta" value="0"/>
+    </stats>
+    <stats xsi:type="cs:NumPostTimeSlot" name="#Posts per time slot">
+      <postsPerTimeSlot value="0"/>
+      <postsPerTimeSlot key="Afternoon" value="0"/>
+      <postsPerTimeSlot key="Evening" value="0"/>
+      <postsPerTimeSlot key="Night" value="0"/>
+    </stats>
+  </cs:District>
+  <cs:District name="NordEst" coordBx="2.0" coordBy="2.0" coordEx="3.0" coordEy="3.0">
+    <stats xsi:type="cs:NumPost" name="#Posts"/>
+    <stats xsi:type="cs:NumPhoto" name="#Pictures"/>
+    <stats xsi:type="cs:NumPostLang" name="#Posts per language">
+      <langNumber value="0"/>
+      <langNumber key="English" value="0"/>
+      <langNumber key="LassaSta" value="0"/>
+    </stats>
+    <stats xsi:type="cs:NumPostTimeSlot" name="#Posts per time slot">
+      <postsPerTimeSlot value="0"/>
+      <postsPerTimeSlot key="Afternoon" value="0"/>
+      <postsPerTimeSlot key="Evening" value="0"/>
+      <postsPerTimeSlot key="Night" value="0"/>
+    </stats>
+  </cs:District>
+  <cs:Place name="Gelateria Finocchi">
+    <stats xsi:type="cs:NumPost" name="#Posts"/>
+    <stats xsi:type="cs:NumPhoto" name="#Pictures"/>
+    <stats xsi:type="cs:NumPostLang" name="#Posts per language">
+      <langNumber value="0"/>
+      <langNumber key="English" value="0"/>
+      <langNumber key="LassaSta" value="0"/>
+    </stats>
+    <stats xsi:type="cs:NumPostTimeSlot" name="#Posts per time slot">
+      <postsPerTimeSlot value="0"/>
+      <postsPerTimeSlot key="Afternoon" value="0"/>
+      <postsPerTimeSlot key="Evening" value="0"/>
+      <postsPerTimeSlot key="Night" value="0"/>
+    </stats>
+  </cs:Place>
+</xmi:XMI>
